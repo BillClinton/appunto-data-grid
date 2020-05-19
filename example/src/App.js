9843c848d3d3e6b1885eda078022e5c2cbb4dd60
BillClinton <bclinton@appunto.net>
2020-05-18 20:23:52 -0400

    Add grid component to example

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import { ThemeProvider, CSSReset, Text } from '@chakra-ui/core'
+import { DataGrid } from 'appunto-data-grid'
+import data from './mock-data.json'
 
-import { ExampleComponent } from 'appunto-data-grid'
-import 'appunto-data-grid/dist/index.css'
+const columns = [
+  {
+    text: 'First Name',
+    dataIndex: 'first_name',
+    span: 3
+  },
+  {
+    text: 'Last Name',
+    dataIndex: 'last_name',
+    span: 3
+  },
+  {
+    text: 'Email',
+    dataIndex: 'email',
+    span: 4
+  },
+  {
+    text: 'City',
+    dataIndex: 'city',
+    span: 2
+  }
+]
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+export default function App() {
+  return (
+    <ThemeProvider>
+      <CSSReset />
+      <div className='App'>
+        <Text textAlign='center' fontSize='sm'>
+          Simple data grid with local pagination using Chakra UI
+        </Text>
+        <DataGrid columns={columns} data={data} />
+      </div>
+    </ThemeProvider>
+  )
 }
-
-export default App
