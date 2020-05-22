e422db5a882816020d9cb0618f0452f0235a9630
BillClinton <bclinton@appunto.net>
2020-05-21 23:50:47 -0400

    Move grid logic to context

--- src/components/datagrid/GridContext.js
+++ src/components/datagrid/GridContext.js
@@ -1,4 +1,7 @@
 import React, { createContext, useContext, useState } from 'react'
+import { Grid } from '@chakra-ui/core'
+import useChakraBreakpoints from './useChakraBreakpoints'
+import usePagination from './usePagination'
 
 export const GridContext = createContext()
 
@@ -7,8 +10,20 @@ export function useGridContext() {
   return gridContext
 }
 
-const GridContextProvider = (props) => {
+const GridContextProvider = ({
+  columns: columnConfig,
+  data: gridData,
+  children,
+  ...rest
+}) => {
   const [selectedId, setSelectedId] = useState(null)
+  const [columns] = useState(columnConfig)
+  const [data, setData] = useState(gridData)
+  const { breakpointIndex } = useChakraBreakpoints()
+  const { next, prev, currentData, currentPage, maxPage } = usePagination(
+    data,
+    10
+  )
 
   const getSpan = (col, breakpointIndex) => {
     return Array.isArray(col.span) ? col.span[breakpointIndex] : col.span
@@ -20,11 +35,36 @@ const GridContextProvider = (props) => {
     }, 0)
   }
 
-  const gridContext = { selectedId, setSelectedId, getSpan, getTotalSpan }
+  const totalSpan = columns.reduce((a, v) => {
+    return a + getSpan(v, breakpointIndex)
+  }, 0)
+
+  const gridContext = {
+    columns,
+    data: currentData(),
+    setData,
+    breakpointIndex,
+    selectedId,
+    setSelectedId,
+    getSpan,
+    totalSpan,
+    pageNext: next,
+    pagePrev: prev,
+    currentPage,
+    maxPage
+  }
 
   return (
     <GridContext.Provider value={gridContext}>
-      {props.children}
+      <Grid
+        gridTemplateColumns={`repeat(${totalSpan}, 1fr [col-start])`}
+        overflow='hidden'
+        minHeight={0}
+        minWidth={0}
+        {...rest}
+      >
+        {children}
+      </Grid>
     </GridContext.Provider>
   )
 }
