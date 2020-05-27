c48a04482bed07934d838db823305fcafda767f5
BillClinton <bclinton@appunto.net>
2020-05-27 19:48:55 -0400

    Add new component exports to package index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import GridContextProvider from './components/datagrid/GridContext'
+import GridContextProvider, {
+  GridContext
+} from './components/datagrid/GridContext'
+import DataGrid from './components/datagrid/DataGrid'
 import GridHeader from './components/datagrid/GridHeader'
+import GridColumnHeader from './components/datagrid/GridColumnHeader'
 import GridRows from './components/datagrid/GridRows'
 import GridFooter from './components/datagrid/GridFooter'
 
-export { GridContextProvider, GridHeader, GridRows, GridFooter }
+export {
+  GridContext,
+  GridContextProvider,
+  DataGrid,
+  GridHeader,
+  GridColumnHeader,
+  GridRows,
+  GridFooter
+}
