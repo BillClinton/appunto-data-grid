a6257cf80b4cab7d26d900b81012f1005dbde8cb
BillClinton <bclinton@appunto.net>
2020-05-27 19:59:04 -0400

    Add tabs to example App.js

--- example/src/App.js
+++ example/src/App.js
@@ -1,57 +1,44 @@
 import React from 'react'
-import { ThemeProvider, CSSReset, Text } from '@chakra-ui/core'
 import {
-  DataGrid,
-  GridContextProvider,
-  GridHeader,
-  GridRows,
-  GridFooter
-} from 'appunto-data-grid'
-import data from './mock-data.json'
-
-const columns = [
-  {
-    text: 'First Name',
-    dataIndex: 'first_name',
-    span: 3
-  },
-  {
-    text: 'Last Name',
-    dataIndex: 'last_name',
-    span: 3
-  },
-  {
-    text: 'Email',
-    dataIndex: 'email',
-    span: [0, 4, 4, 4]
-  },
-  {
-    text: 'City',
-    dataIndex: 'city',
-    span: [0, 0, 2, 2]
-  }
-]
+  ThemeProvider,
+  CSSReset,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel
+} from '@chakra-ui/core'
+import RemoteStoreProvider from './remote/RemoteStore'
+import LocalGrid from './LocalGrid'
+import RemoteGrid from './remote/RemoteGrid'
+import { GridContextProvider } from 'appunto-data-grid'
 
 export default function App() {
   return (
     <ThemeProvider>
       <CSSReset />
       <div className='App'>
-        <GridContextProvider
-          // Column definition and data
-          columns={columns}
-          data={data}
-          // Chakra UI props forwarded to <Grid> component
-          mx={[0, 4, 6, 8]}
-          my={[0, 2, 2, 2]}
-          borderRadius={8}
-          border='2px'
-          borderColor='gray.200'
-        >
-          <GridHeader bg='gray.200' fontWeight='medium' />
-          <GridRows />
-          <GridFooter bg='gray.200' mb='-1px' />
-        </GridContextProvider>
+        <Tabs defaultIndex={1}>
+          <TabList>
+            <Tab>Local Grid</Tab>
+            <Tab>Remote Data Grid</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <GridContextProvider>
+                <LocalGrid />
+              </GridContextProvider>
+            </TabPanel>
+            <TabPanel>
+              <RemoteStoreProvider>
+                <GridContextProvider>
+                  <RemoteGrid />
+                </GridContextProvider>
+              </RemoteStoreProvider>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </div>
     </ThemeProvider>
   )
