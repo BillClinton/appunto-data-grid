b01d76c4601c14804e39ee606fb518d09cecdc52
BillClinton <bclinton@appunto.net>
2020-05-27 19:35:10 -0400

    Begin to adapt to remote data source WIP

--- src/components/datagrid/GridContext.js
+++ src/components/datagrid/GridContext.js
@@ -1,7 +1,6 @@
 import React, { createContext, useContext, useState } from 'react'
-import { Grid } from '@chakra-ui/core'
 import useChakraBreakpoints from './useChakraBreakpoints'
-import usePagination from './usePagination'
+// import usePagination from './usePagination'
 
 export const GridContext = createContext()
 
@@ -10,62 +9,44 @@ export function useGridContext() {
   return gridContext
 }
 
-const GridContextProvider = ({
-  columns: columnConfig,
-  data: gridData,
-  children,
-  ...rest
-}) => {
+const GridContextProvider = ({ columns: colCfg, data: gridData, children }) => {
   const [selectedId, setSelectedId] = useState(null)
-  const [columns] = useState(columnConfig)
+  const [columns, setColumns] = useState(colCfg)
   const [data, setData] = useState(gridData)
   const { breakpointIndex } = useChakraBreakpoints()
-  const { next, prev, currentData, currentPage, maxPage } = usePagination(
-    data,
-    10
-  )
+  // const { next, prev, currentData, currentPage, maxPage } = usePagination(
+  //   data,
+  //   10
+  // )
 
   const getSpan = (col, breakpointIndex) => {
     return Array.isArray(col.span) ? col.span[breakpointIndex] : col.span
   }
 
-  const getTotalSpan = (columns, breakpointIndex) => {
-    return columns.reduce((a, v) => {
+  const totalSpan =
+    columns &&
+    columns.reduce((a, v) => {
       return a + getSpan(v, breakpointIndex)
     }, 0)
-  }
-
-  const totalSpan = columns.reduce((a, v) => {
-    return a + getSpan(v, breakpointIndex)
-  }, 0)
 
   const gridContext = {
     columns,
-    data: currentData(),
+    setColumns,
+    data,
     setData,
     breakpointIndex,
     selectedId,
     setSelectedId,
     getSpan,
-    totalSpan,
-    pageNext: next,
-    pagePrev: prev,
-    currentPage,
-    maxPage
+    totalSpan
+    // pageNext: next,
+    // pagePrev: prev,
+    // currentPage,
+    // maxPage
   }
 
   return (
-    <GridContext.Provider value={gridContext}>
-      <Grid
-        gridTemplateColumns={`repeat(${totalSpan}, 1fr [col-start])`}
-        overflow='hidden'
-        minHeight={0}
-        minWidth={0}
-        {...rest}
-      >
-        {children}
-      </Grid>
-    </GridContext.Provider>
+    <GridContext.Provider value={gridContext}>{children}</GridContext.Provider>
   )
 }
 
